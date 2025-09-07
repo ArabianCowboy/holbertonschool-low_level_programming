11a64d477b27825847585e924e219c5e8612e773
root <root@ip-10-42-227-131.ec2.internal>
2025-09-07 13:31:46 +0000

    Task 1: add swap_int function

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -2,16 +2,14 @@
 
 /**
  * swap_int - swaps the values of two integers
- * @a: pointer to first integer
- * @b: pointer to second integer
- *
- * Return: nothing
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
  */
 void swap_int(int *a, int *b)
 {
-    int temp;
+	int temp;
 
-    temp = *a;
-    *a = *b;
-    *b = temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
