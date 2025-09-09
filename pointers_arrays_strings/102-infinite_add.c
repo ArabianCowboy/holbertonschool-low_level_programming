b828e87dcef1c6bd6b41228d16b15b4517ff9288
Arabiancowboy <midoxp@gmail.com>
2025-09-09 17:48:54 +0000

    Fix 102-infinite_add.c: write result at r[0], handle buffer size, Betty tabs

--- pointers_arrays_strings/102-infinite_add.c
+++ pointers_arrays_strings/102-infinite_add.c
@@ -4,49 +4,63 @@
  * infinite_add - adds two positive numbers stored as strings
  * @n1: first number (as string)
  * @n2: second number (as string)
- * @r: buffer to store the result
- * @size_r: size of buffer r
+ * @r: buffer to store the result (result must start at r[0])
+ * @size_r: size of buffer r (including '\0')
  *
- * Return: pointer to result r, or 0 if it cannot be stored in r
+ * Return: pointer to r on success, or 0 if it cannot be stored in r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, k, carry = 0, d1, d2, sum;
+	int i = 0, j = 0, write, carry = 0, d1, d2, sum;
+	int start, end, idx;
+
+	if (size_r < 2)
+		return (0);
 
 	while (n1[i] != '\0')
 		i++;
 	while (n2[j] != '\0')
 		j++;
 
-	/* k is index where we place the null-terminator (end of string) */
-	k = size_r - 1;
-	r[k] = '\0';
-	k--;
+	/* end is index of final '\0'; write is last digit position */
+	end = size_r - 1;
+	r[end] = '\0';
+	write = end - 1;
 
 	i--;
 	j--;
 
 	while (i >= 0 || j >= 0 || carry)
 	{
-		if (k < 0)
+		if (write < 0)
 			return (0);
 
 		d1 = (i >= 0) ? n1[i] - '0' : 0;
 		d2 = (j >= 0) ? n2[j] - '0' : 0;
-		if (d1 < 0 || d1 > 9 || d2 < 0 || d2 > 9)
+
+		/* reject non-digits (inputs must be positive integers) */
+		if ((i >= 0 && (d1 < 0 || d1 > 9)) ||
+		    (j >= 0 && (d2 < 0 || d2 > 9)))
 			return (0);
 
 		sum = d1 + d2 + carry;
-		r[k] = (sum % 10) + '0';
+		r[write] = (sum % 10) + '0';
 		carry = sum / 10;
 
 		i--;
 		j--;
-		k--;
+		write--;
 	}
 
-	/* shift result to the start of buffer if needed */
-	k++;
-	return (r + k);
+	/* shift result to r[0].. */
+	start = write + 1;
+	idx = 0;
+	while (r[start] != '\0')
+	{
+		r[idx++] = r[start++];
+	}
+	r[idx] = '\0';
+
+	return (r);
 }
 
