0ccfcddca5e1ebffceffcdea1148d69eb1ef2c17
Arabiancowboy <midoxp@gmail.com>
2025-09-09 17:26:09 +0000

    Add 0-strcat.c: implement _strcat function

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,23 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    /* find end of dest */
-    while (dest[i] != '\0')
-        i++;
+	/* find end of dest */
+	while (dest[i] != '\0')
+		i++;
 
-    /* copy src into dest */
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	/* copy src into dest */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    /* add null terminator */
-    dest[i] = '\0';
+	/* add null terminator */
+	dest[i] = '\0';
 
-    return (dest);
+	return (dest);
 }
