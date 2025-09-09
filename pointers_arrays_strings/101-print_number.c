6ce12e3f849bc14dc60c050ca2e7711ceaab6070
Arabiancowboy <midoxp@gmail.com>
2025-09-09 17:42:18 +0000

    Add 101-print_number.c: implement print_number and update main.h

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n == -2147483648)
+		{
+			_putchar('2');
+			print_number(147483648);
+			return;
+		}
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+}
+
