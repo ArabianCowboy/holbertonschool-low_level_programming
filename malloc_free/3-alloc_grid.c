632472a44251065f978acce7ef4192f4dd3f5a9b
root <root@ip-10-42-233-99.ec2.internal>
2025-09-17 13:40:47 +0000

    malloc_free: implement 3-alloc_grid (Task 3) + header prototype

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2D array of integers
+ * @width: number of columns
+ * @height: number of rows
+ *
+ * Return: pointer to 2D array on success,
+ *         NULL on failure or if width/height <= 0
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			/* free previously allocated rows */
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+
+		/* initialize all cells to 0 */
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
+}
+
