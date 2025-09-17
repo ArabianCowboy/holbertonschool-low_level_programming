5f0b2a12316e5c70214e7540147b380e03976b3a
root <root@ip-10-42-233-99.ec2.internal>
2025-09-17 13:37:00 +0000

    malloc_free: implement 1-strdup (Task 1) + header prototype and READMEs

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ *           which contains a copy of the string given as a parameter.
+ * @str: source string
+ *
+ * Return: pointer to duplicated string on success,
+ *         NULL if str is NULL or if memory allocation fails.
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int len = 0, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	/* compute length manually (no strlen allowed) */
+	while (str[len] != '\0')
+		len++;
+
+	dup = malloc((len + 1) * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+	dup[len] = '\0';
+
+	return (dup);
+}
+
