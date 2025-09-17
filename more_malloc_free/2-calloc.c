2623c932ebbb616d6b1ea4da98e44d8fef32d126
root <root@ip-10-42-233-99.ec2.internal>
2025-09-17 14:05:25 +0000

    0x0B: Implement 2-calloc.c and update main.h

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - Allocates memory for an array using malloc and sets to zero.
+ * @nmemb: Number of elements.
+ * @size: Size of each element.
+ *
+ * Return: Pointer to allocated memory initialized to zero.
+ *         NULL if nmemb or size is 0, or if malloc fails.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+
+	return (p);
+}
+
