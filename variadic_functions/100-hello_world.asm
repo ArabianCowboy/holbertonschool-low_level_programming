303b51995987d3c3e8511080c2a94fcc213befe5
ArabianCowboy <midoxp@gmail.com>
2025-09-25 15:27:33 +0000

    Fix: 64-bit assembly Hello, World with correct binary name

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,17 +1,17 @@
 section .data
-    msg db "Hello, World", 10    ; the string + newline
-    len equ $ - msg              ; length of the string
+    msg db "Hello, World", 10    ; string with newline
+    len equ $ - msg              ; length of string
 
 section .text
     global _start
 
 _start:
-    mov rax, 1        ; syscall number for write
-    mov rdi, 1        ; file descriptor (stdout)
+    mov rax, 1        ; syscall: write
+    mov rdi, 1        ; fd = stdout
     mov rsi, msg      ; pointer to string
     mov rdx, len      ; length of string
     syscall
 
-    mov rax, 60       ; syscall number for exit
+    mov rax, 60       ; syscall: exit
     xor rdi, rdi      ; exit code 0
     syscall
