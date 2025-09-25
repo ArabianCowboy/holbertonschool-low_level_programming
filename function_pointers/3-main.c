3d227873182b8856430995ea5494868873121935
ArabianCowboy <midoxp@gmail.com>
2025-09-25 15:16:37 +0000

    Fix: Day 19 Task 3 with Betty style and documentation

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,35 +3,40 @@
 #include <stdlib.h>
 
 /**
- * main - simple calculator
+ * main - performs simple operations
  * @argc: argument count
  * @argv: argument vector
+ *
+ * Description: program that takes 3 arguments:
+ * num1 operator num2, and prints the result.
+ * If the number of arguments is incorrect, print Error and exit 98.
+ * If the operator is invalid, print Error and exit 99.
+ * If user divides by 0, print Error and exit 100.
  *
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*op)(int, int);
+	int num1, num2, result;
+	int (*op)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    op = get_op_func(argv[2]);
-    if (!op)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	op = get_op_func(argv[2]);
+	if (!op)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	result = op(num1, num2);
 
-    result = op(num1, num2);
-    printf("%d\n", result);
-
-    return (0);
+	printf("%d\n", result);
+	return (0);
 }
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -2,37 +2,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * op_add - returns the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
 int op_add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
 
+/**
+ * op_sub - returns the difference of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
+ */
 int op_sub(int a, int b)
 {
-    return (a - b);
+	return (a - b);
 }
 
+/**
+ * op_mul - returns the product of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
+ */
 int op_mul(int a, int b)
 {
-    return (a * b);
+	return (a * b);
 }
 
+/**
+ * op_div - returns the result of division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: division of a by b
+ */
 int op_div(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    return (a / b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
 }
 
+/**
+ * op_mod - returns the remainder of division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of a divided by b
+ */
 int op_mod(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    return (a % b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
