141a6d3cc2238c37815db2bbc18ffa66bc55a0aa
Arabian <midoxp@gmail.com>
2025-10-16 14:57:54 +0000

    Fix: wrap comment lines under 80 chars for Betty; task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - deletes the node at a given index of a dlistint_t list
+ * delete_dnodeint_at_index - deletes a node at a given index
  * @head: double pointer to the head of the list
  * @index: index of the node to delete (starts at 0)
  *
@@ -18,7 +18,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	node = *head;
 
-	/* Case 1: delete head (index 0) */
+	/* delete head (index 0) */
 	if (index == 0)
 	{
 		*head = node->next;
@@ -28,18 +28,18 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	/* Traverse to the node at position `index` */
+	/* move to the node at position `index` */
 	while (node != NULL && i < index)
 	{
 		node = node->next;
 		i++;
 	}
 
-	/* Index out of range */
+	/* index out of range */
 	if (node == NULL)
 		return (-1);
 
-	/* Relink neighbors to skip `node` */
+	/* relink neighbors to skip `node` */
 	if (node->next)
 		node->next->prev = node->prev;
 	if (node->prev)
