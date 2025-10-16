72d548d154eed8bff0f8025cd066cc08885f1a25
Arabian <midoxp@gmail.com>
2025-10-16 14:36:00 +0000

    Task 7: Insert node at a given index in a doubly linked list

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: double pointer to the head of the list
+ * @idx: index at which to insert the new node (starts at 0)
+ * @n: data value for the new node
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *temp;
+	unsigned int i = 0;
+
+	if (h == NULL)
+		return (NULL);
+
+	/* Case 1: insert at the beginning */
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	temp = *h;
+	while (temp != NULL && i < idx - 1)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	/* If index is out of range */
+	if (temp == NULL)
+		return (NULL);
+
+	/* Case 2: inserting at the end */
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	/* Case 3: inserting in the middle */
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = temp->next;
+	new_node->prev = temp;
+
+	temp->next->prev = new_node;
+	temp->next = new_node;
+
+	return (new_node);
+}
+
