9b630c54a8d950933e545d72b7da1befa8509ea3
Arabian <midoxp@gmail.com>
2025-10-23 13:19:55 +0000

    Task 1: Implement djb2 hash function

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -33,3 +33,7 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 
 #endif /* HASH_TABLES_H */
+
+/* Task 1 */
+unsigned long int hash_djb2(const unsigned char *str);
+
