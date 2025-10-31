ac57470826858c14c20ecab3a10f682396df09ba
ArabinCowBoy <midoxp@gmail.com>
2025-10-31 11:58:59 +0000

    Update main.h with prototypes for tasks 0-2

--- file_io/main.h
+++ file_io/main.h
@@ -7,6 +7,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+/* Task 0 */
 ssize_t read_textfile(const char *filename, size_t letters);
+
+/* Task 1 */
+int create_file(const char *filename, char *text_content);
+
+/* Task 2 */
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
